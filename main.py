75c4f032e5f3e812bbb096337647b3febea899af
qwert7661 <bentleykennedystone@gmail.com>
2025-10-28 09:44:45 -0500

    added word count, word pairs. word pairs needs fixing

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ if len(sys.argv) != 2:
 
 
 
-from stats import get_num_words
-from stats import count_characters
-from stats import sort_counts
+from stats import *
+
+
 
 def get_file_text(filepath):
     with open(filepath, encoding="utf-8") as f:
@@ -22,16 +22,48 @@ def main():
     print(f"Analyzing book found at {filepath}...")
     full_text_string = get_file_text(filepath)
     
-    print("----------- Word Count ----------")
+    print("-------Total WordCount ----------")
     num_words = get_num_words(full_text_string)
     print(f"Found {num_words} total words")
 
     print("--------- Character Count -------")
     character_counts = count_characters(full_text_string)
     sorted_counts = sort_counts(character_counts)
-    
     for item in sorted_counts:
         for key in item:
             print(f"{key}: {item[key]}")
+    
+    print("-------Individual Word Count-----")
+    word_counts = count_words(full_text_string)
+    sorted_words = sort_counts(word_counts)
+    counter = 0
+    for item in sorted_words:
+        for key in item:
+            print(f"{key:<17}: {item[key]:>4}",end=' | ')
+            counter += 1
+            if counter > 8: 
+                counter = 0
+                print()
+
+
+    word_pairs = get_word_pairs(full_text_string)
+    counted_word_pairs = count_word_pairs(word_pairs)
+    sorted_word_pair_counts = sort_counts_for_word_pairs(counted_word_pairs)
+    trimmed_word_pair_counts = remove_unique_word_pairs(sorted_word_pair_counts)
+    
+    print("-------- Non-Unique Word Pairs ---")
+    counter = 0
+    for item in trimmed_word_pair_counts:
+        for key in item:
+            print(f"{key:<30}: {item[key]:>3}",end=' | ')
+            counter += 1
+            if counter > 5:
+                counter = 0
+                print()
+    
+    # print(trimmed_word_pair_counts)
+
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -12,15 +12,96 @@ def count_characters(string):
         character_counts[c] += 1
     return character_counts
 
+def count_words(string):
+    word_counts = {}
+    string = string.lower()
+    split_string = string.split()
+    for w in split_string:
+        if w not in word_counts:
+            word_counts[w] = 0
+        word_counts[w] += 1
+    return word_counts
+
+def get_word_pairs(string):
+    
+    def remove_backslash_n(string):
+        string = string.replace("\n", " ")
+        return string
+    
+    string = remove_backslash_n(string)
+    string = string.lower()
+    
+    cleaned_string = ''
+    for c in string:
+        if c.isalpha() or c == ' ' or c == "’" or c == "'" or c == "-" or c == '‟':
+            cleaned_string += c
+    
+
+    word_pairs = []
+    word_pair = ''
+    space_counter = 0
+    for c in cleaned_string:
+        if c == " ":
+            space_counter += 1
+        if space_counter < 2:
+            word_pair += c
+        else:
+            space_counter = 0
+            word_pairs.append(word_pair)
+            word_pair = ''
+    
+    def remove_blank_word_pairs(word_pairs):
+        true_word_pairs = []
+        
+        for item in word_pairs:
+            is_word_pair = False
+            for c in item:
+                if c != ' ' and c != '’' and c != "'" and c != "-" and c != '‟':
+                    is_word_pair = True
+            if is_word_pair:
+                true_word_pairs.append(item)
+        return true_word_pairs
+    word_pairs = remove_blank_word_pairs(word_pairs)
+    
+    return word_pairs
+
+def count_word_pairs(word_pairs):
+    word_pair_counts = {}
+    for pair in word_pairs:
+        if pair not in word_pair_counts:
+            word_pair_counts[pair] = 0
+        word_pair_counts[pair] += 1
+    return word_pair_counts
+
 def sort_counts(dic):
-    unkeyed_counts = []
-    list_of_dic = []
+    list_from_dic = []
     
     for key in dic:
-        unkeyed_counts.append(dic[key])
-    unkeyed_counts.sort(reverse=True)
-    for n in unkeyed_counts:
+        if key.isalpha():
+            list_from_dic.append({key:dic[key]})
+
+    def get_count(item):
+        for k in item:
+            return item[k]
+
+    list_from_dic.sort(reverse=True, key=get_count)
+
+    return(list_from_dic)
+
+def sort_counts_for_word_pairs(dic):
+    list_from_dic = []
+    for key in dic:
+        list_from_dic.append({key:dic[key]})
+    def get_count(item):
+        for k in item:
+            return item[k]
+    list_from_dic.sort(reverse=True, key=get_count)
+    return(list_from_dic)
+
+def remove_unique_word_pairs(lis):
+    trimmed = []
+    for dic in lis:
         for key in dic:
-            if dic[key] == n and key.isalpha():
-                list_of_dic.append({key:n})
-    return list_of_dic
+            if dic[key] > 1:
+                trimmed.append({key:dic[key]})
+    return trimmed
